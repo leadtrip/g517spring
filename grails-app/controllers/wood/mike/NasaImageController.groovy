c57b7e9c3a5dec557d117eba7a9ec3e84eacb192
leadtrip <woodm@objectcomputing.com>
2022-05-15 08:46:22 +0100

    Testing

--- grails-app/controllers/wood/mike/NasaImageController.groovy
+++ grails-app/controllers/wood/mike/NasaImageController.groovy
@@ -2,7 +2,7 @@ package wood.mike
 
 class NasaImageController {
 
-    def nasaImageService
+    def nasaImageService      // not a standard grails service, see resources.groovy
 
     def index() {
         [nasaImage: nasaImageService.fetch()]
